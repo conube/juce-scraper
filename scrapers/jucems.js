9670bf3bff8b8f25548647f8c09665269f1635a0
Gabriel Marquez <gabriel@conube.com.br>
2015-07-04 16:04:02 -0300

    fix jucems rejected status

--- scrapers/jucems.js
+++ scrapers/jucems.js
@@ -3,6 +3,10 @@
  *
  * @description Scraper for junta comercial of Mato Grosso do Sul
  *              https://jucems.ms.gov.br
+ *
+ * TRAMITACAO - https://servicos.jucems.ms.gov.br/scripts/consulta.exe/posicao?nrproc=140045511
+ * EXIGENCIA - https://servicos.jucems.ms.gov.br/scripts/consulta.exe/posicao?nrproc=140045902
+ * APROVADO - https://servicos.jucems.ms.gov.br/scripts/consulta.exe/posicao?nrproc=140045821
  */
 
 var
@@ -22,20 +26,19 @@ module.exports = {
     var getP = request.getAsync(url);
 
     // scrap the status text
-    var getStatusP = getP.then(function (result) {
+    return getP.then(function (result) {
 
       var status = result[0].body.toLowerCase();
 
       if (status.indexOf('aprovado') >= 0) {
         return response.approved(processNumber, url);
-      } else if (status.indexOf('o existe') >= 0) {
-        return response.notFound(processNumber, url);
       } else if (status.indexOf('em tramitacao') >= 0) {
         return response.waiting(processNumber, url);
-      } else {
+      } else if (status.indexOf('em exig') >= 0) {
         return response.rejected(processNumber, url);
       }
 
+      return response.notFound(processNumber, url);
     });
 
   }
