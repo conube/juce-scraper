21974f8df85f708d4a5179e118cdd6f902d8fa45
FernandoGOT <fernando.1f@hotmail.com>
2015-07-07 19:41:04 -0300

    update

---
+++ scrapers/jucec.js
@@ -0,0 +1,57 @@
+/**
+ * JUCEMG Scraper
+ *
+ * @description Scraper for junta comercial of Minas Gerais
+ *              http://www.jucemg.mg.gov.br/
+ */
+
+var
+  response = require('./base'),
+  request = require('request'),
+  cheerio = require('cheerio'),
+  Promise = require('bluebird');
+
+Promise.promisifyAll(request);
+
+module.exports = {
+  scrap: function(processNumber) {
+
+    // url of the request
+    var url = 'http://vpn2.jucec.ce.gov.br/cdp/cdp.php';
+
+    // split number to use on url
+    var number = processNumber.substring(2, processNumber.length - 1);
+    var year = processNumber.substring(0, 2);
+    var digit = processNumber.substring(processNumber.length - 1, processNumber.length);
+
+    // request for POST
+    var postP = request.postAsync({url:url, form:{protocolo: year + '/' + number + '-' + digit, submit: 'Consultar'}});
+
+    // scrap the status text
+    var postStatusP = postP.then(function (result) {
+
+      $ = cheerio.load(result[0].body);
+
+      return $('.p_viab .situacao').text().toLowerCase();
+
+    });
+
+    // send response
+    return postStatusP.then(function (statusText) {
+
+      if (statusText.indexOf('deferida') >= 0) {
+        return response.approved(processNumber, url);
+        // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=0536492&ano=15&digito=3
+      } else if (statusText.indexOf('exigencia') >= 0) {
+        return response.rejected(processNumber, url);
+        // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=0540885&ano=15&digito=0
+      } else if (statusText.length === 1) {
+        return response.waiting(processNumber, url);
+        // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=053534&ano=15&digito=3
+      }
+
+      return response.notFound(processNumber, url);
+
+    });
+  }
+};
--- scrapers/juceg.js
+++ scrapers/juceg.js
@@ -1,81 +1,112 @@
 /**
  * JUCEMG Scraper
  *
- * @description Scraper for junta comercial of Para
- *              http://servicos.juceg.go.gov.br/
+ * @description Scraper for junta comercial of Goiás
+ *              http://servicos.juceg.go.gov.br/andamento-processo/
  */
 
 var
   response = require('./base'),
   request = require('request'),
   cheerio = require('cheerio'),
-  Promise = require('bluebird');
+  Promise = require('bluebird'),
+  tough = require('tough-cookie');
 
 Promise.promisifyAll(request);
 
 module.exports = {
   scrap: function(processNumber) {
 
+    // url for GET
     var getUrl = 'http://servicos.juceg.go.gov.br/andamento-processo';
+
+    // url for POST
+    var postUrl = 'http://servicos.juceg.go.gov.br/andamento-processo/interface/principal.xhtml';
+
+    // split number to use on url
+    var number = processNumber.substring(2, processNumber.length - 1);
+    var year = processNumber.substring(0, 2);
+    var digit = processNumber.substring(processNumber.length - 1, processNumber.length);
+
+    // ID from 
+    var id;
 
     // request for POST
     var getP = request.getAsync(getUrl);
 
+    // get url to save the ID and cookie
     var getViewState = getP.then(function (result) {
       $ = cheerio.load(result[0].body);
+      var fullCookie = result[0].headers['set-cookie'][0].split(';')[0];
 
-      // console.log('test', result[0].body);
+      var idList = $('input');
 
-      var idLis = $('input');
-      var id;
-
-      for (var i = idLis.length - 1; i >= 0; i--) {
-        if ($(idLis[i]).val().length >= 35) {
-          id = $(idLis[i]).val()
-
+      // set the ID
+      for (var i = idList.length - 1; i >= 0; i--) {
+        if ($(idList[i]).val().length >= 35) {
+          id = $(idList[i]).val()
           break;
         };
-
       };
+      return fullCookie;
     })
 
-    // // split number to use on url
-    // var number = processNumber.substring(2, processNumber.length - 1);
-    // var year = processNumber.substring(0, 2);
-    // var digit = processNumber.substring(processNumber.length - 1, processNumber.length);
+    // set the cookie
+    var setCook = getViewState.then(function (fullCookie) {
 
-    // var postUrl = 'http://servicos.juceg.go.gov.br/andamento-processo/interface/principal.xhtml';
+      var j = request.jar();
+      var cookie = request.cookie(fullCookie);
+      j.setCookie(fullCookie, getUrl);
 
-    // // request for POST
-    // var postP = request.postAsync({url:url, form:{nrProtocolo: year + '/' + number + '-' + digit, formPrincipal: 'formPrincipal', btnConsularProcesso: 'btnConsularProcesso' }});
+      return j;
+    })
 
-    // // scrap the status text
-    // var postStatusP = postP.then(function (result) {
+    // request for POST
+    var postP = setCook.then(function (cookies) {
+      return request.postAsync({
+        url:postUrl, 
+        form:{
+          'javax.faces.partial.ajax': 'true',
+          'javax.faces.source': 'btnConsularProcesso',
+          'javax.faces.partial.execute': 'btnConsularProcesso nrProtocolo',
+          'javax.faces.partial.render': 'formPrincipal formPrincipal pnBotoes mensagem',
+          btnConsularProcesso: 'btnConsularProcesso',
+          formPrincipal: 'formPrincipal',
+          nrProtocolo: year + '/' + number + '-' + digit,
+          'javax.faces.ViewState': id },
+        jar: cookies
+      });
+    })
+    
 
-    //   $ = cheerio.load(result[0].body);
+    // scrap the status text
+    var postStatusP = postP.then(function (result) {
 
-    //   // console.log('result', $('.ui-datatable-even').text().toLowerCase())
+      $ = cheerio.load(result[0].body);
 
-    //   return $('.ui-datatable-even:last-of-type').text().toLowerCase();
+      return result[0].body.toLowerCase();
 
-    // });
+    });
 
-    // // send response
-    // return postStatusP.then(function (statusText) {
+    // send response
+    return postStatusP.then(function (statusText) {
 
-    //   if (statusText.indexOf('pronto') >= 0 || statusText.indexOf('arquivado') >= 0) {
-    //     return response.approved(processNumber, url);
-    //     // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=0536492&ano=15&digito=3
-    //   } else if (statusText.indexOf('exigencia') >= 0) {
-    //     return response.rejected(processNumber, url);
-    //     // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=0540885&ano=15&digito=0
-    //   } else if (statusText.length === 1) {
-    //     return response.waiting(processNumber, url);
-    //     // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=053534&ano=15&digito=3
-    //   }
+      if (statusText.indexOf('bt_aprovado') >= 0) {
+        return response.approved(processNumber, getUrl);
+        // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=0536492&ano=15&digito=3
+      } else if (statusText.indexOf('bt_exigencia') >= 0) {
+        return response.rejected(processNumber, getUrl);
+        // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=0540885&ano=15&digito=0
+      }else if (statusText.indexOf('bt_transporte_ida') >= 0) {
+        return response.waiting(processNumber, getUrl);
+        // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=0540885&ano=15&digito=0
+      } else if (statusText.length === 1) {
+        return response.waiting(processNumber, getUrl);
+        // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=053534&ano=15&digito=3
+      }
 
-    //   return response.notFound(processNumber, url);
+      return response.notFound(processNumber, getUrl);
 
-    // });
+    });
   }
 };
