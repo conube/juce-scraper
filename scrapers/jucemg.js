7812ffd968d4fab03a408c2c7ae6d950d2886706
Gabriel Marquez <gabriel@conube.com.br>
2015-08-21 09:19:59 -0300

    fix jucemg scrapper

--- scrapers/jucemg.js
+++ scrapers/jucemg.js
@@ -19,29 +19,33 @@ module.exports = {
     var url = 'http://www.jucemg.mg.gov.br/ibr/concprocessos';
 
     // request for POST
-    var postP = request.postAsync({url:url, form:{numprotocolo: processNumber, continuar: '1', tokenConsultaProc: ''}});
+    var postP = request.postAsync({
+      url: url,
+      form: {
+        numprotocolo: processNumber,
+        continuar: '1',
+        tokenConsultaProc: ''
+      }
+    });
 
     // scrap the status text
     var postStatusP = postP.then(function (result) {
 
       $ = cheerio.load(result[0].body);
 
-      return $('.p_viab .situacao').text().toLowerCase();
+      return ($('.situacao').text() || "") .toLowerCase().trim();
 
     });
 
     // send response
     return postStatusP.then(function (statusText) {
 
-      if (statusText.indexOf('deferida') >= 0) {
+      if (statusText.indexOf('deferida') >= 0 || statusText.indexOf('conclu') >= 0) {
         return response.approved(processNumber, url);
-        // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=0536492&ano=15&digito=3
-      } else if (statusText.indexOf('exigencia') >= 0) {
+      } else if (statusText.indexOf('exig') >= 0) {
         return response.rejected(processNumber, url);
-        // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=0540885&ano=15&digito=0
-      } else if (statusText.length === 1) {
+      } else if (statusText.indexOf('pendente') >= 0) {
         return response.waiting(processNumber, url);
-        // https://www.jucesp.sp.gov.br/eprotocolo2.asp?numero=053534&ano=15&digito=3
       }
 
       return response.notFound(processNumber, url);
